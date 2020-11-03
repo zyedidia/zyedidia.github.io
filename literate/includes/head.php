ebf42cf765fbe7fc4416691060fb0b81245f2b25
Zachary Yedidia <zyedidia@gmail.com>
2020-11-03 15:58:30 -0500

    Change links to https

--- literate/includes/head.php
+++ literate/includes/head.php
@@ -5,11 +5,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <!-- Include jquery -->
-<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
 
 <!-- Include Bootstrap -->
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://bootswatch.com/spacelab/bootstrap.min.css">
 
 <!-- Include my css -->
